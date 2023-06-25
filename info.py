537ffa62bffc4b5c648cb8e1ff0d27d2c724ec0f
githubforstreamlit <136007419+githubforstreamlit@users.noreply.github.com>
2023-06-25 17:01:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -14,8 +14,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ['API_ID', '22097375'])
 API_HASH = environ['API_HASH', '7c2b72b9c3f2bbbdf7f3f0557f5a7374']
-BOT_TOKEN = environ['BOT_TOKEN', '6061964545:AAEH5y1xKmMS-dm0gq91M6x2tEhYrXNWRl0
-']
+BOT_TOKEN = environ['BOT_TOKEN', '6061964545:AAHp3jWPMk-NJUBCbLCHoNI50fal4yhWewg']
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
